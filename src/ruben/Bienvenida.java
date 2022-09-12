3c08160c5fe0eabf5fd36777779995d218afe288
RUBEN <RUBEN@DESKTOP-1AOS2E2>
2022-09-11 20:29:04 -0500

    Version1

---
+++ src/ruben/Bienvenida.java
@@ -0,0 +1,16 @@
+package ruben;
+/**
+ * @author RUBEN
+ *
+ */
+public class Bienvenida {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+
+}
